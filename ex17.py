2a26015425f18688835d2b9cb207848cfb05ef77
Dheeraj Kumar Ketireddy <dheeru0198@gmail.com>
2012-04-10 19:05:34 +0530

    Upto Exercise17

---
+++ ex17.py
@@ -0,0 +1,21 @@
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print "Coping from %s to %s" % (from_file, to_file)
+
+indata = open(from_file).read()
+
+print "The input file is %d bytes long" % len(indata)
+
+print "Does the output file exist? %r" % exists(to_file)
+print "Ready, hit RETURN to continue, CTRL-C to abort."
+raw_input()
+
+output = open(to_file, 'w')
+output.write(indata)
+
+print "Alright, all done."
+
+output.close()
