5da81f547e019062583ff7412add31e4f4fa99eb
Dheeraj Kumar Ketireddy <dheeru0198@gmail.com>
2012-04-10 13:22:51 +0530

    Second Exercise

---
+++ ex2.py
@@ -0,0 +1,15 @@
+# In computer programming, a comment is a programming language construct used to embed programmer-readable annotations in the source code of a computer program.
+
+#Comments are usually added with the purpose of making the source code easier to understand.
+
+# In Python the comments are placed after '#'.
+
+# Anything after the # is ignored by python.
+
+print "This is my second exercise in python" # from the book "Learn Python the hard way.
+
+# Comments can be used to hide code from the interpreter for testing.
+
+# print "Hidden code"
+
+print "This is visible to interpreter"
